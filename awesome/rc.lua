635b31c9284fe9938fcd10d4170184d085276954
Greg Flynn <gregflynn42@gmail.com>
2017-06-13 07:42:26 -0400

    floating window toggle is no longer finger yoga

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -511,12 +511,13 @@ clientkeys = gears.table.join(
       {description = "toggle fullscreen", group = "client"}),
     awful.key({ modkey,           }, "q",      function (c) c:kill()                         end,
               {description = "close", group = "client"}),
-    awful.key({ modkey, "Shift" }, "f",  awful.client.floating.toggle                     ,
-              {description = "toggle floating", group = "client"}),
+    awful.key({ modkey, }, "a", function(c)
+      awful.client.floating.toggle(c)
+      c.maximized_vertical = false
+      c.maximized_horizontal = false
+    end, {description = "toggle floating", group = "client"}),
     awful.key({ modkey, "Control" }, "Return", function (c) c:swap(awful.client.getmaster()) end,
               {description = "move to master", group = "client"}),
-    awful.key({ modkey,           }, "o",      function (c) c:move_to_screen()               end,
-              {description = "move to screen", group = "client"}),
     awful.key({ modkey,           }, "t",      function (c) c.ontop = not c.ontop            end,
               {description = "toggle keep on top", group = "client"}),
     awful.key({ modkey,           }, "n",
@@ -528,6 +529,8 @@ clientkeys = gears.table.join(
         {description = "minimize", group = "client"}),
     awful.key({ modkey,           }, "m",
         function (c)
+            c.maximized_vertical = false
+            c.maximized_horizontal = false
             c.maximized = not c.maximized
             c:raise()
         end ,
