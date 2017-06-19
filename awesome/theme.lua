d56ffc7c18500edeab6a4d054c98494c54506783
Greg Flynn <gregflynn42@gmail.com>
2017-06-19 10:20:58 -0400

    new wallpaper!

--- awesome/theme.lua
+++ awesome/theme.lua
@@ -87,7 +87,7 @@ theme.titlebar_maximized_button_focus_inactive  = themes_path.."zenburn/titlebar
 theme.titlebar_maximized_button_normal_active = themes_path.."zenburn/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active  = themes_path.."zenburn/titlebar/maximized_focus_active.png"
 
-theme.wallpaper = "/home/greg/Dropbox/Wallpapers/any_minute_now_UltraHD.jpg"
+theme.wallpaper = "/home/greg/Dropbox/Wallpapers/serenity_UltraHD.jpg"
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = themes_path.."zenburn/layouts/fairh.png"
