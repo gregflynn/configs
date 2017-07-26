e71e4eede879bb33dc9cc38d5d04d55936a644f9
Greg Flynn <gregflynn42@gmail.com>
2017-07-26 08:32:35 -0400

    lain and twitter emoji p[ackage aur fixes

--- bashrc_helpers/pacman.sh
+++ bashrc_helpers/pacman.sh
@@ -153,7 +153,9 @@ function aur_update_single() {
   if [ "$lver" != "$aver" ]; then
     echo "Upgrading $C1$1$C0: $C2$lver$C0 => $C3$aver$C0"
     git checkout master
-    git clean -fd
+    if [ "$1" == "lain-git" ]; then
+      rm -rf lain
+    fi
     makepkg -si
   else
     echo "Already up-to-date: $C1$1$C0: $C3$lver$C0"
@@ -194,18 +196,18 @@ function aur_version() {
   if [ ! -e $pkgbuild ]; then
     return 1
   fi
-  ver=`cat $pkgbuild | grep "pkgver=" | sed 's/pkgver=//' | sed 's/"//g' | sed "s/'//g"`
-  rel=`cat $pkgbuild | grep "pkgrel=" | sed 's/pkgrel=//' | sed 's/"//g' | sed "s/'//g"`
-  epo=`cat $pkgbuild | grep "epoch=" | sed 's/epoch=//' | sed 's/"//g' | sed "s/'//g"`
+  ver=$(cat $pkgbuild | grep "pkgver=" | head -n 1 | sed 's/pkgver=//' | sed 's/"//g' | sed "s/'//g")
+  rel=$(cat $pkgbuild | grep "pkgrel=" | sed 's/pkgrel=//' | sed 's/"//g' | sed "s/'//g")
+  epo=$(cat $pkgbuild | grep "epoch=" | sed 's/epoch=//' | sed 's/"//g' | sed "s/'//g")
 
   # needed for lain-git
   if [[ $ver == *"\$pkgcom"* ]]; then
-    pkgcom=`cat $pkgbuild | grep "pkgcom=" | sed 's/pkgcom=//' | sed 's/"//g' | sed "s/'//g"`
+    pkgcom=$(cat $pkgbuild | grep "pkgcom=" | sed 's/pkgcom=//' | sed 's/"//g' | sed "s/'//g")
     ver=$(echo $ver | sed "s/\$pkgcom/$pkgcom/")
   fi
 
   if [[ $ver == *"\$pkgsha"* ]]; then
-    pkgsha=`cat $pkgbuild | grep "pkgsha=" | sed 's/pkgsha=//' | sed 's/"//g' | sed "s/'//g"`
+    pkgsha=$(cat $pkgbuild | grep "pkgsha=" | sed 's/pkgsha=//' | sed 's/"//g' | sed "s/'//g")
     ver=$(echo $ver | sed "s/\$pkgsha/$pkgsha/")
   fi
 
