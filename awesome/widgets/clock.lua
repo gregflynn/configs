8d6df676136887cdcec38e29d135a28bc0b2d518
Greg Flynn <gregflynn42@gmail.com>
2017-11-29 07:36:16 -0500

    remove calendar widget hover

--- awesome/widgets/clock.lua
+++ awesome/widgets/clock.lua
@@ -8,16 +8,18 @@ local clock = wibox.widget.textclock(
     '<span color="'..beautiful.fg_minimize..'">%a %b %e %l:%M%P</span>'
 )
 
-lain.widget.calendar {
-    attach_to = { clock },
-    icons = '',
-    notification_preset = {
-        font = 'Hack',
-        fg = beautiful.fg_normal,
-        bg = beautiful.bg_normal
-    },
-    cal = "/usr/bin/env TERM=linux /usr/bin/cal --color=always"
-}
+-- Calendar widget on hover
+-- NOTE: commented out because it gets stuck on a lot, vvv annoying
+-- lain.widget.calendar {
+--     attach_to = { clock },
+--     icons = '',
+--     notification_preset = {
+--         font = 'Hack',
+--         fg = beautiful.fg_normal,
+--         bg = beautiful.bg_normal
+--     },
+--     cal = "/usr/bin/env TERM=linux /usr/bin/cal --color=always"
+-- }
 
 clock:buttons(gears.table.join(
     -- NOTE: this kills lain's buttons
