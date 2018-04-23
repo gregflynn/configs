fbaac5e86322a38e841e8e864c96adbcb11292fb
Greg Flynn <gregflynn42@gmail.com>
2018-04-22 20:52:20 -0400

    update default background

--- awesome/theme.lua
+++ awesome/theme.lua
@@ -115,7 +115,7 @@ theme.titlebar_maximized_button_focus_inactive  = themes_path.."zenburn/titlebar
 theme.titlebar_maximized_button_normal_active = themes_path.."zenburn/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active  = themes_path.."zenburn/titlebar/maximized_focus_active.png"
 
-theme.wallpaper = "/home/greg/Dropbox/Wallpapers/heaven.jpg"
+theme.wallpaper = "/home/greg/Dropbox/Wallpapers/sky_mirror_UltraHD.jpg"
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = themes_path.."zenburn/layouts/fairh.png"
