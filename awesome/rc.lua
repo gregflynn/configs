a3a1447a3e5bf42360e759b9f99cb6c4461b36f5
Greg Flynn <gregflynn42@gmail.com>
2018-10-20 16:54:55 -0400

    switch alacritty to default terminal

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -18,7 +18,7 @@ awesome.set_preferred_icon_size(42)
 local dpi       = beautiful.xresources.apply_dpi
 local sep       = lain.util.separators
 local colors    = beautiful.colors
-local terminal  = "tilix"
+local terminal  = "alacritty"
 local taglist   = { "main", "alpha", "bravo", "slack", "music" }
 local ipairs    = ipairs
 
