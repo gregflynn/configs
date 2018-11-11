7e3799a99559c3e7febe65404738ec8120ff3081
Greg Flynn <gregflynn42@gmail.com>
2018-11-11 15:33:18 -0500

    don't start nm-applet,redshift

--- x11/xprofile.sh
+++ x11/xprofile.sh
@@ -9,15 +9,6 @@ if command -v nvidia-settings > /dev/null; then
     nvidia-settings --assign GPULogoBrightness=0
 fi
 
-# startup applications
-if command -v nm-applet > /dev/null; then
-    nm-applet&
-fi
-
-if command -v redshift > /dev/null; then
-    redshift&
-fi
-
 if command -v xmodmap > /dev/null; then
     xmodmap_loc='{DS_HOME}/x11/xmodmap'
     if [ -f ${xmodmap_loc} ]; then
