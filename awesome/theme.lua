09c033987194fa25faef715a476bd01984f58485
Greg Flynn <gregflynn42@gmail.com>
2018-12-02 19:43:12 -0500

    update wallpaper

--- awesome/theme.lua
+++ awesome/theme.lua
@@ -36,7 +36,7 @@ local theme = {
     border_width  = dpi(2),
     font          = font_name.." 10",
     font_name     = font_name,
-    wallpaper     = dotsan_home.."/private/wallpapers/close_to_the_sun.jpg",
+    wallpaper     = dotsan_home.."/private/wallpapers/light_lanes.jpg",
     border_shape  = function(cr, w, h)
         gears.shape.rounded_rect(cr, w, h, dpi(5))
     end,
