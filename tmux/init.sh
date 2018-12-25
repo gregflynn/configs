8eee105989e7c4193b94e865638fa4e53578dc09
Greg Flynn <gregflynn42@gmail.com>
2018-12-25 17:21:39 -0500

    install and update tmux plugins on dotsan install

--- tmux/init.sh
+++ tmux/init.sh
@@ -13,9 +13,8 @@ function __dotsan__tmux__init {
             esac
             ;;
         build)
-            if [ ! -e ${__tmux__plugins} ]; then
-                git clone https://github.com/tmux-plugins/tpm \
-                        ${__tmux__plugins}
+            if [[ ! -e ${__tmux__plugins} ]]; then
+                git clone https://github.com/tmux-plugins/tpm ${__tmux__plugins}
             else
                 pushd ${__tmux__plugins} > /dev/null
                 git pull
@@ -24,6 +23,8 @@ function __dotsan__tmux__init {
             ;;
         install)
             __dotsan__link tmux tmux.conf .tmux.conf
+            bash ${__tmux__plugins}/bin/install_plugins
+            bash ${__tmux__plugins}/bin/update_plugins all
             ;;
     esac
 }
