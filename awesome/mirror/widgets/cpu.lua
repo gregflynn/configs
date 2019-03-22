6fe71f10901b6b4bddb8222f88faf7c110a05c56
Greg Flynn <gregflynn42@gmail.com>
2019-03-22 10:13:03 -0400

    add load graph for gpu

--- awesome/mirror/widgets/cpu.lua
+++ awesome/mirror/widgets/cpu.lua
@@ -4,9 +4,30 @@ local wibox     = require("wibox")
 
 local vicious = require("vicious")
 
+local file = require("util/file")
+local FontIcon = require("util/fonticon")
+local text = require("util/text")
+
 local colors = beautiful.colors
 local dpi    = beautiful.xresources.apply_dpi
 
+
+local function make_graph()
+    local graph = wibox.widget.graph {
+        width            = dpi(40),
+    }
+    graph.background_color = colors.red
+    graph.color = colors.background
+    return graph
+end
+
+local function graph_container(widget, tooltip_text)
+    local container = wibox.container.margin(widget, dpi(2), dpi(2), dpi(2), dpi(2))
+    awful.tooltip { objects = {container}, text = tooltip_text }
+    return container
+end
+
+local container = wibox.widget { layout = wibox.layout.fixed.horizontal }
 
 local cpu_temp_widget = awful.widget.watch("sensors", 15, function(widget, stdout)
     local temp = stdout:match("Package id 0:%s+%p(%d+%p%d)")
@@ -15,33 +36,42 @@ local cpu_temp_widget = awful.widget.watch("sensors", 15, function(widget, stdou
     end
 
     if temp then
-        widget:set_markup(
-            string.format(
-                '<span color="%s">%s°C</span> ',
-                colors.background,
-                math.floor(temp)
-            )
-        )
+        widget:set_markup(string.format('<span color="%s">%s°C</span> ', colors.background, math.floor(temp)))
     end
 end)
 awful.tooltip {
     objects = {cpu_temp_widget},
     text = "CPU Temp"
 }
+if file.exists("/usr/bin/nvidia-smi") then
+    container:add(FontIcon { icon = "\u{fb19}", color = colors.background })
+end
+container:add(cpu_temp_widget)
 
-local cpu_load_widget = wibox.widget.graph {
-    width = dpi(40),
-}
-cpu_load_widget.background_color = colors.red
-cpu_load_widget.color = colors.background
+local cpu_load_widget = make_graph()
 vicious.register(cpu_load_widget, vicious.widgets.cpu, "$1")
-awful.tooltip {
-    objects = {cpu_load_widget},
-    text = "CPU Load"
-}
+container:add(graph_container(cpu_load_widget, "CPU Load"))
 
-return wibox.widget {
-    layout = wibox.layout.fixed.horizontal,
-    cpu_temp_widget,
-    wibox.container.margin(cpu_load_widget, dpi(2), dpi(2), dpi(2), dpi(2))
-}
+if file.exists("/usr/bin/nvidia-smi") then
+    container:add(FontIcon { icon = "\u{f878}", color = colors.background })
+    local gpu_temp_command = "nvidia-smi --format=csv,nounits,noheader --query-gpu=temperature.gpu"
+    local gpu_temp_widget = awful.widget.watch(gpu_temp_command, 15, function(widget, stdout)
+        widget:set_markup(string.format(' <span color="%s">%s°C</span> ', colors.background, text.trim(stdout)))
+    end)
+    container:add(gpu_temp_widget)
+    awful.tooltip {
+        objects = {gpu_temp_widget},
+        text = "GPU Temp"
+    }
+
+    local gpu_load_command = "nvidia-smi --format=csv,nounits,noheader --query-gpu=utilization.gpu"
+    local gpu_load_widget = awful.widget.watch(gpu_load_command, 2, function(widget, stdout)
+        if stdout == nil or tonumber(stdout) == nil then
+            return
+        end
+        widget:add_value(tonumber(stdout) / 100.0)
+    end, make_graph())
+    container:add(graph_container(gpu_load_widget, "GPU Load"))
+end
+
+return container
