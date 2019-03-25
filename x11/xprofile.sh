c78724b1863aacc98267b12822259b5b1d7465d1
Greg Flynn <gregflynn42@gmail.com>
2019-03-25 07:41:59 -0400

    fix compton init for version 6 of compton

--- x11/xprofile.sh
+++ x11/xprofile.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-compton -b -c --backend glx --paint-on-overlay --vsync opengl-swc
+compton -b -c --backend glx --vsync --use-damage
 
 xautolock -time 15 -locker "bash {DS_LOCK}" &
 
