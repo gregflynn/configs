559356097c08bbe958e626bdda7fccdfb2c0750f
Greg Flynn <gregflynn42@gmail.com>
2019-05-27 16:46:37 -0400

    add task warrior module

---
+++ tasks/__init__.py
@@ -0,0 +1,24 @@
+from _src.initializer import BaseInitializer
+
+
+class Initializer(BaseInitializer):
+    @property
+    def requirements(self):
+        return ['task', 'timew']
+
+    @property
+    def install_in_cli(self):
+        return True
+
+    def build(self):
+        self.inject('taskrc', inject_map={
+            'TASK_THEME': self.base_path('tasks.theme')
+        })
+
+    def install(self):
+        self.shell_base('tasks.sh')
+        self.link_dist('taskrc', '.taskrc')
+
+
+def initializer():
+    return Initializer
---
+++ tasks/tasks.sh
@@ -0,0 +1,2 @@
+alias t='task'
+alias tw='timew'
