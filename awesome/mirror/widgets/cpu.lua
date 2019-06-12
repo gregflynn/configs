227e496edb9e4636a0c6b7b72ebf48d5ef131892
Greg Flynn <gregflynn42@gmail.com>
2019-06-11 22:03:49 -0400

    remove cpu widget icon

--- awesome/mirror/widgets/cpu.lua
+++ awesome/mirror/widgets/cpu.lua
@@ -4,7 +4,7 @@ local wibox     = require('wibox')
 
 local vicious = require('vicious')
 
-local FontIcon        = require('util/fonticon')
+-- local FontIcon        = require('util/fonticon')
 local Graph           = require('util/graph')
 local SanityContainer = require('util/sanitycontainer')
 
@@ -12,7 +12,7 @@ local dpi = beautiful.xresources.apply_dpi
 
 
 local color = beautiful.colors.red
-local cpu_icon = FontIcon {icon = '\u{fb19}', color = color}
+-- local cpu_icon = FontIcon {icon = '\u{fb19}', color = color}
 local cpu_temp_widget = awful.widget.watch("sensors", 15, function(widget, stdout)
     local temp = stdout:match("Package id 0:%s+%p(%d+%p%d)")
     if not temp then
@@ -32,7 +32,7 @@ vicious.register(cpu_load_widget, vicious.widgets.cpu, "$1")
 return SanityContainer {
     widget = wibox.widget {
         layout = wibox.layout.fixed.horizontal,
-        cpu_icon,
+        -- cpu_icon,
         cpu_temp_widget,
         cpu_load_widget.container
     },
