4cce144a4e70216122eb94c5a88dc225c98dc71c
Greg Flynn <gregflynn42@gmail.com>
2019-06-11 21:58:24 -0400

    refactor volume into one widget

--- awesome/mirror/widgets/gpmdp.lua
+++ awesome/mirror/widgets/gpmdp.lua
@@ -1,29 +1,22 @@
 local io, os, string, table = io, os, string, table
 
-local awful     = require("awful")
-local beautiful = require("beautiful")
-local gears     = require("gears")
-local naughty   = require("naughty")
-local wibox     = require("wibox")
-local lain      = require("lain")
+local beautiful = require('beautiful')
+local naughty   = require('naughty')
 
-local text     = require("util/text")
-local FontIcon = require("util/fonticon")
+local lain = require('lain')
+
+local music = require('util/music')
 
 local markup = lain.util.markup
 local dpi    = beautiful.xresources.apply_dpi
 local colors = beautiful.colors
 
 
-local gpmdp_album_art_fmt = "/tmp/gpmcover-%s"
-local gpmdp_json = beautiful.home.."/.config/Google Play Music Desktop Player/json_store/playback.json"
 local icon_not_running = '\u{fa7d}'
 local icon_running_paused = '\u{f04c}'
 local icon_running_playing = '\u{f001}'
 
 local gpmdp = {
-    notify        = "on",
-    file_location = gpmdp_json,
     notification_preset = {
         title     = "Now playing",
         icon_size = dpi(128),
@@ -32,39 +25,28 @@ local gpmdp = {
     notification  = nil,
     current_track = nil,
     current_album_art = nil,
-    font_icon = FontIcon {icon = icon_not_running, color = colors.blue},
-    running_counter = 0
 }
 
-local tooltip = awful.tooltip {}
-
-function trunc(str, max_len)
-    return text.trunc(str, max_len, '(', true)
-end
-
-function gpmdp.notification_on()
-    local new_album_art = string.format(gpmdp_album_art_fmt, math.random(0, 3894732897))
-    local gpm_now = gpmdp.latest
-    gpmdp.current_track = gpm_now.title
-
-    awful.spawn.easy_async({"curl", gpm_now.cover_url, "-o", new_album_art}, function()
+function gpmdp.notification_on(current)
+    gpmdp.current_track = current.title
+    music.get_album_art(current, function(album_art_path)
         local old_id
-
+    
         if gpmdp.notification then
             old_id = gpmdp.notification.id
         end
-
+    
         gpmdp.notification = naughty.notify({
             preset = gpmdp.notification_preset,
-            icon = new_album_art,
+            icon = album_art_path,
             replaces_id = old_id
         })
-
+    
         -- clean up album art
         if gpmdp.current_album_art then
             os.remove(gpmdp.current_album_art)
         end
-        gpmdp.current_album_art = new_album_art
+        gpmdp.current_album_art = album_art_path
     end)
 end
 
@@ -74,126 +56,26 @@ function gpmdp.notification_off()
     gpmdp.notification = nil
 end
 
-function gpmdp.get_lines(file)
-    local f = io.open(file)
-    if not f then return
-    else f:close() end
-
-    local lines = {}
-    for line in io.lines(file) do
-        lines[#lines + 1] = line
-    end
-    return lines
-end
-
-function gpmdp.get_now()
-    local gpm_now = { playing = false }
-    local filelines = gpmdp.get_lines(gpmdp.file_location)
-
-    -- exit early if gpmdp isn't running
-    if not filelines then
-        return gpm_now
-    end
-
-    -- parse the json file
-    local json = lain.util.dkjson
-    local dict, _, _ = json.decode(table.concat(filelines), 1, nil)
-
-    -- sometimes dict doesn't parse
-    if not dict then
-        return gpm_now
-    end
-
-    -- pull out all them datas
-    gpm_now.artist = dict.song.artist
-    gpm_now.album = dict.song.album
-    gpm_now.title = dict.song.title
-    gpm_now.cover_url = dict.song.albumArt
-    gpm_now.playing = dict.playing
-    return gpm_now
-end
-
-function gpmdp.render(widget, title, artist, album, is_playing)
-    local font_icon = icon_running_playing
-    -- local title_color = colors.background
-
-    if not is_playing then
-        font_icon = icon_running_paused
-        -- title_color = colors.gray
-    end
-
-    -- update wibar display
-    gpmdp.font_icon:update(font_icon, colors.blue)
-
-    -- update tooltip
-    tooltip.markup = string.format(
+function gpmdp.get_tooltip(current)
+    return string.format(
         "%s\nBy: %s\nFrom: %s",
-        markup.italic(markup.fg.color(colors.white, title)),
-        markup.fg.color(colors.blue, artist),
-        markup.fg.color(colors.purple, markup.italic(album))
+        markup.italic(markup.fg.color(colors.white, current.title)),
+        markup.fg.color(colors.blue, current.artist),
+        markup.fg.color(colors.purple, markup.italic(current.album))
     )
+end
 
-    -- update notification display
+function gpmdp.update_notification(current)
     gpmdp.notification_preset.text = string.format(
         "\n%s\n%s\n%s",
-        markup.fg.color(colors.white, markup.italic(markup.big(title))),
-        markup.fg.color(colors.blue, markup.big(artist)),
-        markup.fg.color(colors.purple, markup.italic(markup.big(album)))
+        markup.fg.color(colors.white, markup.italic(markup.big(current.title))),
+        markup.fg.color(colors.blue, markup.big(current.artist)),
+        markup.fg.color(colors.purple, markup.italic(markup.big(current.album)))
     )
-end
 
-function gpmdp.maybe_not_running(widget)
-    gpmdp.running_counter = gpmdp.running_counter + 1
-    if gpmdp.running_counter > 5 then
-        gpmdp.font_icon:update(icon_not_running, colors.blue)
-        widget:set_markup('')
-        tooltip.text = 'Music'
-        gpmdp.current_track = nil
+    if current.title ~= gpmdp.current_track then
+        gpmdp.notification_on(current)
     end
 end
 
-gpmdp.widget = awful.widget.watch(
-    {"pidof", "Google Play Music Desktop Player"}, 2,
-    function(widget, stdout)
-        local is_running = stdout ~= ''
-
-        if not is_running then
-            gpmdp.maybe_not_running(widget)
-            return
-        end
-
-        local gpm_now = gpmdp.get_now()
-        gpmdp.latest = gpm_now
-
-        if gpm_now.title then
-            gpmdp.running_counter = 0
-
-            local title = text.trim(gpm_now.title)
-            local artist = text.trim(gpm_now.artist)
-            local album = text.trim(gpm_now.album)
-
-            gpmdp.render(widget, title, artist, album, gpm_now.playing)
-
-            if gpmdp.notify == "on" and gpm_now.title ~= gpmdp.current_track then
-                gpmdp.notification_on()
-            end
-        else
-            gpmdp.maybe_not_running(widget)
-        end
-    end
-)
-
-local buttons = gears.table.join(
-    awful.button({ }, 1, function()
-        if gpmdp.current_track ~= nil then
-            gpmdp.notification_on()
-        else
-            awful.spawn("gpmdp")
-        end
-    end)
-)
-
-gpmdp.font_icon:buttons(buttons)
-tooltip:add_to_object(gpmdp.font_icon)
-
-return gpmdp.font_icon
+return gpmdp
--- awesome/mirror/widgets/volume.lua
+++ awesome/mirror/widgets/volume.lua
@@ -4,24 +4,36 @@ local beautiful = require("beautiful")
 local wibox     = require("wibox")
 local gears     = require("gears")
 
-local FontIcon = require("util/fonticon")
+local music           = require('util/music')
+local FontIcon        = require('util/fonticon')
+local SanityContainer = require('util/sanitycontainer')
+local gpmdp           = require('widgets/gpmdp')
 
 local colors = beautiful.colors
 local dpi    = beautiful.xresources.apply_dpi
+local naughty = require('naughty')
 
-local gpmdp = require("widgets/gpmdp")
+
+local font_icon_headphones = "\u{f7ca}"
+local font_icon_mute = "\u{fc5d}"
+local font_icon_low = "\u{f026}"
+local font_icon_med = "\u{f027}"
+local font_icon_high = "\u{f028}"
+local icon_running_paused = '\u{f04c}'
+local icon_running_playing = '\u{f001}'
 
 local tooltip = awful.tooltip {}
 local fg_color = colors.blue
 
+local icon = FontIcon {icon = font_icon_med, color = fg_color}
 local volume = lain.widget.pulsebar {
     width = dpi(50),
     margins = 4,
     paddings = 0,
     notification_preset = {
-        position = "bottom_middle",
-        title    = "volume",
-        font     = "Hack 12"
+        position = 'bottom_middle',
+        title    = 'volume',
+        font     = 'Hack 12'
     },
     colors = {
         background = beautiful.colors.gray,
@@ -29,59 +41,94 @@ local volume = lain.widget.pulsebar {
         unmute     = fg_color
     },
     settings = function()
-        if volume_now.muted == "yes" then
-            tooltip.text = "Muted"
-        else
-            local level = tonumber(volume_now.left)
-            tooltip.text = string.format("Speakers: %s%%", level)
-        end
+        update_volume(volume_now)
     end
-}
-
-volume.buttons = awful.util.table.join(
-    awful.button({}, 1, function() -- left click
-        awful.spawn("pavucontrol")
-    end),
-    awful.button({}, 3, function() -- right click
-        awful.spawn(string.format("pactl set-sink-mute %d toggle", volume.device))
-        volume.update()
-    end),
-    awful.button({}, 4, function() -- scroll up
-        awful.spawn(string.format("pactl set-sink-volume %d +1%%", volume.device))
-        volume.update()
-    end),
-    awful.button({}, 5, function() -- scroll down
-        awful.spawn(string.format("pactl set-sink-volume %d -1%%", volume.device))
-        volume.update()
-    end)
-)
-
-local container = wibox.widget {
-    layout = wibox.layout.fixed.horizontal,
-    gpmdp,
-    wibox.container.margin(volume.bar, dpi(0), dpi(0), dpi(5), dpi(5)),
 }
 volume.bar.shape = beautiful.border_shape
 volume.bar.bar_shape = beautiful.border_shape
-volume.bar:buttons(volume.buttons)
-tooltip:add_to_object(container)
 volume.tooltip:remove_from_object(volume.bar)
 
-container.globalkeys = gears.table.join(
-    awful.key({ }, "XF86AudioRaiseVolume", function()
-        awful.spawn("pactl set-sink-volume @DEFAULT_SINK@ +5%")
-        volume.notify()
-    end),
-    awful.key({ }, "XF86AudioLowerVolume", function()
-        awful.spawn("pactl set-sink-volume @DEFAULT_SINK@ -5%")
-        volume.notify()
-    end),
-    awful.key({ }, "XF86AudioMute", function()
-        awful.spawn("pactl set-sink-mute @DEFAULT_SINK@ toggle")
-        volume.notify()
-    end)
-)
+volume_container = SanityContainer {
+    widget = wibox.widget {
+        layout = wibox.layout.fixed.horizontal,
+        icon,
+        wibox.container.margin(volume.bar, dpi(0), dpi(0), dpi(4), dpi(4)),
+    },
+    color = fg_color,
+    tooltip = 'foo',
+    buttons = gears.table.join(
+        awful.button({}, 1, function() -- left click
+            awful.spawn("pavucontrol")
+        end),
+        awful.button({}, 3, function() -- right click
+            awful.spawn(string.format("pactl set-sink-mute %d toggle", volume.device))
+            volume.update()
+        end),
+        awful.button({}, 4, function() -- scroll up
+            awful.spawn(string.format("pactl set-sink-volume %d +1%%", volume.device))
+            volume.update()
+        end),
+        awful.button({}, 5, function() -- scroll down
+            awful.spawn(string.format("pactl set-sink-volume %d -1%%", volume.device))
+            volume.update()
+        end)
+    ),
+    globalkeys = gears.table.join(
+        awful.key({ }, "XF86AudioRaiseVolume", function()
+            awful.spawn("pactl set-sink-volume @DEFAULT_SINK@ +5%")
+            volume.notify()
+        end),
+        awful.key({ }, "XF86AudioLowerVolume", function()
+            awful.spawn("pactl set-sink-volume @DEFAULT_SINK@ -5%")
+            volume.notify()
+        end),
+        awful.key({ }, "XF86AudioMute", function()
+            awful.spawn("pactl set-sink-mute @DEFAULT_SINK@ toggle")
+            volume.notify()
+        end)
+    )
+}
 
-local real_container = wibox.container.margin(container, 0, beautiful.widget_space, 0, 0)
-real_container.globalkeys = container.globalkeys
-return real_container
+function update_volume(volume_now)
+    music.get_current_track(function(current)
+        local is_muted = volume_now.muted == 'yes'
+
+        -- tooltips
+        local vol_tooltip = 'Muted'
+        if not is_muted then
+            local level = tonumber(volume_now.left)
+            vol_tooltip = string.format('Speakers: %s%%', level)
+        end
+
+        if current then
+            local music_tooltip = gpmdp.get_tooltip(current)
+            volume_container:set_markup(string.format('%s\n\n%s', vol_tooltip, music_tooltip))
+
+            
+        else
+            volume_container:set_markup(vol_tooltip)
+        end
+        
+        -- icon
+        if is_muted then
+            icon:update(font_icon_mute, fg_color)
+        else
+            if current then
+                if current.playing then
+                    icon:update(icon_running_playing, fg_color)
+                else
+                    icon:update(icon_running_paused, fg_color)
+                end
+            else
+                icon:update(font_icon_high, fg_color)
+            end
+        end
+        
+        -- notifications
+        if current then
+            gpmdp.update_notification(current)
+        end
+    end)
+end
+
+return volume_container
