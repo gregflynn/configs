18f27e0fcb9143d7db23cf8a5773d7e4b96e847c
Greg Flynn <gregflynn42@gmail.com>
2019-09-08 20:30:39 -0400

    update brightness widget

--- awesome/mirror/brightness.lua
+++ awesome/mirror/brightness.lua
@@ -7,19 +7,26 @@ local brightness = {
     bright_notification = nil
 }
 
+local tick = "█"
+local tick_pre = "\u{e0b2}"
+local tick_post = "\u{e0b0}"
+local tick_none = " "
+
 function brightness.update()
     awful.spawn.easy_async("light", function(stdout)
         local preset = {
             position = "bottom_middle",
             title    = "brightness",
-            font     = "Hack 12"
+            font     = "Hack 16"
         }
 
         local level = math.modf((tonumber(stdout) / 100) * brightness.width)
         preset.text = string.format(
-            "[%s%s]",
-            string.rep("|", level),
-            string.rep(" ", brightness.width - level)
+            "%s%s%s%s",
+            tick_pre,
+            string.rep(tick, level),
+            string.rep(tick_none, brightness.width - level),
+            tick_post
         )
 
         if not brightness.bright_notification then
@@ -37,14 +44,14 @@ brightness.globalkeys = gears.table.join(
     awful.key(
         { }, "XF86MonBrightnessDown",
         function()
-            awful.spawn("light -U -p 10")
+            awful.spawn("light -U 10")
             brightness.update()
         end
     ),
     awful.key(
         { }, "XF86MonBrightnessUp",
         function()
-            awful.spawn("light -A -p 10")
+            awful.spawn("light -A 10")
             brightness.update()
         end
     )
