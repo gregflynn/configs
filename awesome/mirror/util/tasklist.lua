07cc054d63573319fbe3de7116346f3be2a9597c
Greg Flynn <gregflynn42@gmail.com>
2019-10-27 18:39:19 -0400

    fix previous client color in tasklist

--- awesome/mirror/util/tasklist.lua
+++ awesome/mirror/util/tasklist.lua
@@ -10,9 +10,10 @@ local SanityContainer = require("util/sanitycontainer")
 local colors = beautiful.colors
 local dpi    = beautiful.xresources.apply_dpi
 
-local client_color = colors.gray
-local client_focus_color = colors.yellow
+local client_color           = colors.gray
+local client_focus_color     = colors.yellow
 local client_minimized_color = colors.purple
+local client_previous_color  = colors.white
 local icon_width = 26
 
 --
@@ -86,72 +87,76 @@ local focused_client_container_fonticon = SanityContainer {
     left = true,
 }
 
-local function update_func(window_list, buttons, label, data, clients)
-    window_list:reset()
+local function create_update_function(s)
+    local function update_func(window_list, buttons, label, data, clients)
+        window_list:reset()
 
-    if #clients == 0 then
-        return
-    end
-
-    if not data.client_icons then
-        data["client_icons"] = {}
-    end
-
-    -- window icons
-    local previous_client = awful.client.focus.history.get()
-    for idx, c in ipairs(clients) do
-        local client_icon = c.icon
-        local icon_override = display.get_icon_for_client(c)
-        local color = client_color
-
-        if c == client.focus then
-            color = client_focus_color
-        elseif c.minimized then
-            color = client_minimized_color
-        elseif c == previous_client then
-            color = colors.green
+        if #clients == 0 then
+            return
         end
 
-        local container = data.client_icons[idx] or create_client_window_icon()
-        data.client_icons[idx] = container
-
-        if icon_override or not client_icon then
-            -- no true icon or we've overridden it
-            container.set_font_icon(icon_override, color)
-        else
-            -- going with the real app icon here, display the imagebox
-            container.set_icon(client_icon, color)
+        if not data.client_icons then
+            data["client_icons"] = {}
         end
 
-        container:buttons(awful.widget.common.create_buttons(buttons, c))
-        window_list:add(container)
-    end
+        -- window icons
+        local previous_client = awful.client.focus.history.get(s, 1)
+        for idx, c in ipairs(clients) do
+            local client_icon = c.icon
+            local icon_override = display.get_icon_for_client(c)
+            local color = client_color
 
-    -- focused client
-    if client.focus then
-        local client_name = client.focus.name
-        local color = client_focus_color
+            if c == client.focus then
+                color = client_focus_color
+            elseif c.minimized then
+                color = client_minimized_color
+            elseif c == previous_client then
+                color = client_previous_color
+            end
 
-        local container = focused_client_container_fonticon
+            local container = data.client_icons[idx] or create_client_window_icon()
+            data.client_icons[idx] = container
 
-        -- update the client name and only show if it's focused or last
-        if client_name then
-            focused_client_name:set_markup_silently(string.format(
-                '<span color="%s">%s</span>',
-                color, client_name
-            ))
+            if icon_override or not client_icon then
+                -- no true icon or we've overridden it
+                container.set_font_icon(icon_override, color)
+            else
+                -- going with the real app icon here, display the imagebox
+                container.set_icon(client_icon, color)
+            end
+
+            container:buttons(awful.widget.common.create_buttons(buttons, c))
+            window_list:add(container)
         end
 
-        -- update the tooltip
-        container:set_tooltip(string.format('%s (%s)', client_name, client.focus.class))
+        -- focused client
+        if client.focus then
+            local client_name = client.focus.name
+            local color = client_focus_color
 
-        -- update the container color
-        container:set_color(color == client_color and colors.background or color)
+            local container = focused_client_container_fonticon
 
-        container:buttons(awful.widget.common.create_buttons(buttons, client.focus))
+            -- update the client name and only show if it's focused or last
+            if client_name then
+                focused_client_name:set_markup_silently(string.format(
+                    '<span color="%s">%s</span>',
+                    color, client_name
+                ))
+            end
 
-        window_list:add(container)
+            -- update the tooltip
+            container:set_tooltip(string.format('%s (%s)', client_name, client.focus.class))
+
+            -- update the container color
+            container:set_color(color == client_color and colors.background or color)
+
+            container:buttons(awful.widget.common.create_buttons(buttons, client.focus))
+
+            window_list:add(container)
+        end
     end
+
+    return update_func
 end
 
 local function factory(args)
@@ -163,7 +168,7 @@ local function factory(args)
         buttons = gears.table.join(
             awful.button({ }, 1, client_button)
         ),
-        update_function = update_func,
+        update_function = create_update_function(screen),
         layout = {
             fill_space = false,
             layout  = wibox.layout.fixed.horizontal
