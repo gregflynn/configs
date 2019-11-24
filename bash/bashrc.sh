90eb7d4236c8c391ee2e8899e7467c0068028e15
Greg Flynn <gregflynn42@gmail.com>
2019-11-24 16:51:40 -0500

    shorten up bashrc

--- bash/bashrc.sh
+++ bash/bashrc.sh
@@ -1,18 +1,12 @@
 #! /bin/bash
 
-
 # If not running interactively, don't do anything
 [[ "$-" != *i* ]] && return
-
 
 source "{DS_HOME}/zsh/prompt.zsh"
 
 if [[ -e /usr/share/nvm/init-nvm.sh ]]; then
     source /usr/share/nvm/init-nvm.sh
-fi
-
-if [[ -e "{DS_HOME}/private/bashrc.sh" ]]; then
-    source "{DS_HOME}/private/bashrc.sh"
 fi
 
 {DS_SOURCE}
