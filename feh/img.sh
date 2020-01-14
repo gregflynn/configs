d6bb0215fc9804d7030ff6d945c07dd65dddfea8
Greg Flynn <gregflynn42@gmail.com>
2020-01-14 17:10:40 -0500

    fix paths with spaces

--- feh/img.sh
+++ feh/img.sh
@@ -5,7 +5,7 @@ _base_feh="feh -Tview --title %n"
 if [[ "$1" == "" ]]; then
     $_base_feh -Tview
 elif [ -d "$1" ]; then
-    $_base_feh -Tview $1
+    $_base_feh -Tview "$1"
 else
-    $_base_feh -Tview --start-at $1
+    $_base_feh -Tview --start-at "$1"
 fi
