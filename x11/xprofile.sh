ba2c671838b3d1cf5dae4fa177a05682b54ef9f2
Greg Flynn <gregflynn42@gmail.com>
2020-04-04 14:30:37 -0400

    updates for alacritty 0.4.2

--- x11/xprofile.sh
+++ x11/xprofile.sh
@@ -7,6 +7,7 @@ picom --config '{DS_HOME}/x11/picom.conf' -b
 xautolock -notify 60 -detectsleep -time 15 -locker "bash {DS_LOCK}" &
 
 export WINIT_HIDPI_FACTOR=1
+export WINIT_X11_SCALE_FACTOR=1
 
 if command -v xmodmap > /dev/null; then
     xmodmap_loc='{DS_HOME}/x11/xmodmap'
