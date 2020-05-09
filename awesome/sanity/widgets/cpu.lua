2f0e59fc0a44ddf643d09263a473ed6c8f87d570
Greg Flynn <gregflynn42@gmail.com>
2020-05-09 09:57:03 -0400

    remove top output from cpu/mem widgets

--- awesome/sanity/widgets/cpu.lua
+++ awesome/sanity/widgets/cpu.lua
@@ -4,9 +4,7 @@ local vicious   = require('vicious')
 local Container = require('sanity/util/container')
 local Graph     = require('sanity/util/graph')
 local display   = require('sanity/util/display')
-local text      = require('sanity/util/text')
 
-local spawn  = require('awful.spawn')
 local watch  = require('awful.widget.watch')
 local markup = require('lain.util.markup')
 local widget = require('wibox.widget')
@@ -16,12 +14,6 @@ local color  = colors.red
 
 local cpu_container
 local cpu_load_widget = Graph {color = color}
-
-local cpu_cmd = 'top -b -n 1 -o %CPU | tail -n +7 | head -n 6 | awk \'{$1="";$2="";$3="";$4="";$5="";$6="";$7="";$8="";$10="";$11=""; print $0 }\''
-
-local newline           = '\n'
-local space             = ' '
-local tooltip_lines     = {}
 
 local cpu_temp_widget = watch('sensors', 5, function(w, stdout)
     local temp = stdout:match('Package id 0:%s+%p(%d+%p%d)')
@@ -34,35 +26,6 @@ local cpu_temp_widget = watch('sensors', 5, function(w, stdout)
     end
 
     w:set_markup(markup.fg.color(color, string.format('%s°C', math.floor(temp))))
-
-    spawn.easy_async_with_shell(cpu_cmd, function(s)
-        local lines     = text.split(s, newline)
-        local top_index = 1
-
-        for i=1, #lines do
-            if i == #lines then
-                -- last line is empty
-                break
-            end
-
-            if i > 1 then
-                tooltip_lines[top_index] = newline
-                top_index = top_index + 1
-            end
-
-            tooltip_lines[top_index] = space
-            if i == 1 then
-                tooltip_lines[top_index+1] = markup.fg.color(color, text.trim(lines[i]))
-            else
-                tooltip_lines[top_index+1] = text.trim(lines[i])
-            end
-            tooltip_lines[top_index+2] = space
-            top_index = top_index + 3
-        end
-
-        local top = table.concat(tooltip_lines, '', 1, top_index-1)
-        cpu_container:set_tooltip_color(' Processor ', top)
-    end)
 end)
 
 vicious.register(cpu_load_widget, vicious.widgets.cpu, '$1', graph_interval)
@@ -75,5 +38,6 @@ cpu_container = Container {
     },
     color = color,
 }
+cpu_container:set_tooltip_color(' Processor ')
 
 return cpu_container
--- awesome/sanity/widgets/mem.lua
+++ awesome/sanity/widgets/mem.lua
@@ -7,7 +7,6 @@ local number    = require('sanity/util/number')
 local timer     = require('sanity/util/timer')
 
 local spawn  = require('awful.spawn')
-local markup = require('lain.util.markup')
 
 local color   = colors.yellow
 local mem_pie = Pie {color = color, icon = ''}
@@ -19,11 +18,6 @@ local memory_container = Container {
 }
 
 local mem_cmd = 'free -b | grep Mem | awk \'{print $2,$3}\''
-local top_cmd = 'top -b -n 1 -o %MEM | tail -n +7 | head -n 6 | awk \'{$1="";$2="";$3="";$4="";$5="";$6="";$7="";$8="";$9="";$11=""; print $0 }\''
-
-local tooltip_lines = {}
-local newline       = '\n'
-local space         = ' '
 
 timer.loop(5, function()
     spawn.easy_async_with_shell(mem_cmd, function(mem_stdout)
@@ -35,36 +29,12 @@ timer.loop(5, function()
 
         mem_pie:update(used_raw_pct)
 
-        spawn.easy_async_with_shell(top_cmd, function(top_stdout)
-            local lines = text.split(top_stdout, newline)
-            local index = 1
-
-            for i=1, #lines do
-                if i == #lines then
-                    -- last line is empty
-                    break
-                end
-
-                tooltip_lines[index] = newline
-                tooltip_lines[index+1] = space
-                if i == 1 then
-                    tooltip_lines[index+2] = markup.fg.color(color, text.trim(lines[i]))
-                else
-                    tooltip_lines[index+2] = text.trim(lines[i])
-                end
-                tooltip_lines[index+3] = space
-                index = index + 4
-            end
-
-            local top = table.concat(tooltip_lines, '', 1, index-1)
-            memory_container:set_tooltip_color(' Memory ', string.format(
-                ' %s%% Used \n %sB/%sB \n%s',
-                pct_used,
-                number.human_bytes(used_bytes, 2),
-                number.human_bytes(total_bytes, 2),
-                top
-            ))
-        end)
+        memory_container:set_tooltip_color(' Memory ', string.format(
+            ' %s%% Used \n %sB/%sB',
+            pct_used,
+            number.human_bytes(used_bytes, 2),
+            number.human_bytes(total_bytes, 2)
+        ))
     end)
 end)
 
