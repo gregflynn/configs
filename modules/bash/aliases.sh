7bfdcbd458b5b5c3e6383d44467703940b3bd895
Greg Flynn <gregflynn42@gmail.com>
2020-06-30 22:01:39 -0400

    extra

--- modules/bash/aliases.sh
+++ modules/bash/aliases.sh
@@ -29,3 +29,27 @@ fknd() {
     grep --color --include="*.java" --exclude="R.java" -rli "$1" .
     grep --color --include="*.xml" -rli "$1" .
 }
+
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+
+
+if [[ -e "$HOME/.pyenv" ]]; then
+    export PYENV_VIRTUALENV_DISABLE_PROMPT=1
+    eval "$(pyenv init -)"
+    if [[ -e "$HOME/.pyenv/plugins/pyenv-virtualenv" ]]; then
+        eval "$(pyenv virtualenv-init -)"
+    fi
+fi
+
+
+rmpyc() {
+    find . -name '*.pyc' -exec rm -rf {} \;
+    find . -name __pycache__ -exec rm -rf {} \;
+}
+
+alias pygrep='grep --color --include="*.py"'
+
+fynd() {
+    grep --color --include="*.py" -rli "$1" .
+}
